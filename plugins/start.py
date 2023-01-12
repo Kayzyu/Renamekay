edf05809db0d7ea7affe96187eba194d968dbc56
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2023-01-13 03:59:13 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -15,12 +15,12 @@ from helper.database import daily as daily_
 from helper.date import check_expi
 import os
 
-CHANNEL = os.environ.get('CHANNEL', "")
-STRING = os.environ.get("STRING", "")
-ADMIN = int(os.environ.get("ADMIN", 1484670284))
-bot_username = os.environ.get("BOT_USERNAME","GangsterBaby_renamer_BOT")
-log_channel = int(os.environ.get("LOG_CHANNEL", ""))
-token = os.environ.get('TOKEN', '')
+CHANNEL = os.environ.get('CHANNEL', "-1001561165376")
+STRING = os.environ.get("STRING", "BQAhIHQAnFZVgxCv4JNP9LKtr8CI4zv18rr9cbwGZN1drv7rEMI-bXJF0y64NNpMD5xeBQFOpNOHloV2Xs30LnxhlWExq2Kil7550NV_Wrp9wtBA6g1yFiuGPWYsgyKYfQN43_ikeHu8ygd0ZoMrs2TsdHtt64iCWAn7ovHkraho-bDTWBzY7Rcb97o0pfRDRpQJQvC9BkbKu_2cMno0QqpURU998MBdBvX4WuRsuQ4W3oKlfxQK77ttJqmzU3xOhspW0WZByiwiRPVNqYyDiBpJOJzg0_q_q3uFC6MhFoIaf2OcwUqkdb_Qnhp636tOueG1PIorp0Pafy8JjwS5rc2xwB8qwQAAAAB-SFukAA")
+ADMIN = int(os.environ.get("ADMIN", 2118671268))
+bot_username = os.environ.get("BOT_USERNAME","RenamerThubnailBot")
+log_channel = int(os.environ.get("LOG_CHANNEL", "-1001561165376"))
+token = os.environ.get('TOKEN', '5979614687:AAHbj6UprKT9hq7jSOSBMS4Ln683EwSEdJg')
 botid = token.split(':')[0]
 FLOOD = 500
 LAZY_PIC = os.environ.get("LAZY_PIC", "")
